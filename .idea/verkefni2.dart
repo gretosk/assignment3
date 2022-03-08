1e9cd117f293eee1ec370087eeb5cff8bbdc7ba8
gretosk <101219020+gretosk@users.noreply.github.com>
2022-03-08 23:46:00 +0000

    Update verkefni2.dart

--- .idea/verkefni2.dart
+++ .idea/verkefni2.dart
@@ -1,7 +1,8 @@
-
 void main(){
-  
-
+  print('Enter Number');
+  String readLineA = stdin.readLineSync(encoding: Encoding.getByName('UTF-8'));
+  int numberA = int.tryParse(readLineA);
+  print(readLineA);
 
 
 
