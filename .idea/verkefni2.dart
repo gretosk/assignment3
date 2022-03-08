9ef4ab5a6f856a804d22eafc2cfc8856964842f3
gretosk <101219020+gretosk@users.noreply.github.com>
2022-03-08 23:57:40 +0000

    Update verkefni2.dart

--- .idea/verkefni2.dart
+++ .idea/verkefni2.dart
@@ -4,10 +4,18 @@ import 'dart:io';
 void main(List<String> arguments) {
 
 
-  print('Enter Number');
-  String readLineA = stdin.readLineSync(encoding:Encoding.getByName('UTF-8'));
+
+  print('Enter Number A');
+  String readLineA = stdin.readLineSync(encoding: Encoding.getByName('UTF-8'));
   int numberA = int.tryParse(readLineA);
   print(readLineA);
+
+  if (numberA.isNegative){
+    print('$numberA is Negative');
+  } else {
+    print('$numberA is positive');
+  }
+
 
 
 
