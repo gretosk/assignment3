e05a202f8495a8fc87387af7a24c54e7f02de451
gretosk <101219020+gretosk@users.noreply.github.com>
2022-03-08 23:50:22 +0000

    Update verkefni2.dart

--- .idea/verkefni2.dart
+++ .idea/verkefni2.dart
@@ -1,4 +1,8 @@
-void main(){
+import 'dart:convert';
+import 'dart:io';
+
+void main(List<String> arguments){
+
   print('Enter Number');
   String readLineA = stdin.readLineSync(encoding: Encoding.getByName('UTF-8'));
   int numberA = int.tryParse(readLineA);
